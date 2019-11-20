9f2f9226c4ea8b0d7c070768bb2dbb1701b374f8
Rhearepe18 <54646480+Rhearepe18@users.noreply.github.com>
2019-11-19 21:57:11 -0500

    Update Fe 595 A1 Rhea.py

--- Fe 595 A1 Rhea.py
+++ Fe 595 A1 Rhea.py
@@ -1,28 +1,13 @@
-
-# coding: utf-8
-
-# In[2]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-# In[10]:
-
 
 x = np.arange(0,4*np.pi,0.2)   # start,stop,step  
 sine = np.sin(x)
 
 
-# In[15]:
-
-
 plt.plot(x,sine)
 plt.show()
 
-
-# In[16]:
 
 
 cos = np.cos(x)
